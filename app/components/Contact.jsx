f2ae95a2e2ead31025de3425c12b1fe13ec48959
Nandish <nandish1729@gmail.com>
2025-08-28 18:01:57 +0530

    fix contact form api added links

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,51 +1,52 @@
 export default (first) => {
-    return (
-        <div className=" flex flex-col items-center justify-center px-6">
-            <div className="max-w-md w-full p-4">
-                {/* Heading */}
-                <h2 className="text-4xl font-bold text-center mb-6">Get in Touch</h2>
-                <p className="text-center mb-7">
-                    Open to new opportunities, collaborations, and projects. Whether it’s a full-time role or freelance work, feel free to reach out.
-                </p>
+  return (
+    <div className=" flex flex-col items-center justify-center px-6">
+      <div className="max-w-md w-full p-4">
+        <h2 className="text-4xl font-bold text-center mb-6">Get in Touch</h2>
+        <p className="text-center mb-7">
+          Open to new opportunities, collaborations, and projects. Whether it’s
+          a full-time role or freelance work, feel free to reach out.
+        </p>
 
-                {/* Contact Form */}
-                <form className="space-y-6">
+        <form
+          className="space-y-6"
+          action="https://formspree.io/f/mgvlvjqe"
+          method="POST"
+        >
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder=""
+              className="mt-1 block w-full rounded-lg border border-gray-300 p-3 focus:ring-2 focus:ring-black focus:outline-none"
+              required
+            />
+          </div>
 
-                    <div>
-                        <label className="block text-sm font-medium text-gray-700">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            placeholder=""
-                            className="mt-1 block w-full rounded-lg border border-gray-300 p-3 focus:ring-2 focus:ring-black focus:outline-none"
-                            required
-                        />
-                    </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Message
+            </label>
+            <textarea
+              rows="5"
+              name="message"
+              placeholder="Write your message..."
+              className="mt-1 block w-full rounded-lg border border-gray-300 p-3 focus:ring-2 focus:ring-black focus:outline-none"
+              required
+            ></textarea>
+          </div>
 
-                    <div>
-                        <label className="block text-sm font-medium text-gray-700">
-                            Message
-                        </label>
-                        <textarea
-                            rows="5"
-                            placeholder="Write your message..."
-                            className="mt-1 block w-full rounded-lg border border-gray-300 p-3 focus:ring-2 focus:ring-black focus:outline-none"
-                            required
-                        ></textarea>
-                    </div>
-
-                    <button
-                        type="submit"
-                        className="w-full bg-black/80 text-white font-medium py-3 rounded-lg hover:bg-black transition"
-                    >
-                        Send Message
-                    </button>
-                </form>
-
-                {/* Extra Contact Info */}
-
-            </div>
-        </div>
-    )
-}
+          <button
+            type="submit"
+            className="w-full bg-black/80 text-white font-medium py-3 rounded-lg hover:bg-black transition"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
-import Image from "next/image";
 import Screen from "./components/Screen";
 
 export default function Home() {
   return (
     <div className="relative">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#e5e7eb_1px,transparent_4px),linear-gradient(to_bottom,#e5e7eb_1px,transparent_4px)] bg-[size:40px_40px]"></div>
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#e5e7eb_3px,transparent_1px),linear-gradient(to_bottom,#e5e7eb_3px,transparent_1px)] bg-[size:200px_200px]"></div>
       <div className="relative">
         <Screen />
       </div>
     </div>
-
   );
 }
